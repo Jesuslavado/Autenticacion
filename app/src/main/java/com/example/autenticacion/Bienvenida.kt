9a6b998624e7e53d8864e89b546cbec798cb1925
Jesuslavado <kjblpo@gmail.com>
2023-12-20 09:39:21 +0100

    16c

--- app/src/main/java/com/example/autenticacion/Bienvenida.kt
+++ app/src/main/java/com/example/autenticacion/Bienvenida.kt
@@ -1,11 +1,25 @@
 package com.example.autenticacion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.autenticacion.databinding.ActivityBienvenidaBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Bienvenida : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bienvenida)
+    val binding=ActivityBienvenidaBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+        title="Bienvenida"
+        val extras=intent.extras
+        val nombre=extras?.getString("NombreUsuario")
+        binding.textView.text="Bienvenido, $nombre"
+
+        binding.bcerra.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
