bca434c8d0e30fb43d083fd4d005be00edbff83d
Jesuslavado <kjblpo@gmail.com>
2024-01-10 09:36:06 +0100

    18

--- app/src/main/java/com/example/autenticacion/Registrar.kt
+++ app/src/main/java/com/example/autenticacion/Registrar.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import com.example.autenticacion.databinding.ActivityMainBinding
 import com.example.autenticacion.databinding.ActivityRegistrarBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Registrar : AppCompatActivity() {
 
@@ -17,6 +18,8 @@ class Registrar : AppCompatActivity() {
         setContentView(binding.root)
         binding.bregistra.setOnClickListener{
             // COMPROBAMOS Q NO ESTA VACIO
+            val db= FirebaseFirestore.getInstance()
+
             if(binding.nombre.text.isNotEmpty() && binding.apellidos.text.isNotEmpty() && binding.correoE.text.isNotEmpty() && binding.contrasena.text.isNotEmpty())
             {
                 FirebaseAuth.getInstance().createUserWithEmailAndPassword(
@@ -24,6 +27,12 @@ class Registrar : AppCompatActivity() {
                 )
                     .addOnCompleteListener{
                         if(it.isSuccessful){
+                            db.collection("usuarios").document(binding.correoE.text.toString())
+                                .set(mapOf(
+                                    "nombre" to binding.nombre.text.toString(),
+                                    "apellido" to binding.apellidos.text.toString()
+                                ))
+
                             startActivity(Intent(this, Bienvenida::class.java))
                         }
                         else{
